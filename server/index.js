150ad03ecf85a7d586e0953577040a108c2a8694
Manish Yadav <manishyadav14127@gmail.com>
2025-04-14 00:30:48 +0530

    server issues 3

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.options('*', cors()); // Enable preflight for all routes
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://talksy1.netlify.app","http://localhost:5173"],
+    origin: ["https://talksy1.netlify.app","http://localhost:5173"],
     methods: ["GET", "POST"]
   },
   pingInterval: 20000,   // Time between ping messages in ms
